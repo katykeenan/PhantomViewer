2f3f589d352094cc77a20ec2c74a3b38d52c8b14
katykeenan <katy.keenan@gmail.com>
2015-05-13 15:42:59 -0600

    T1 model development
    T1 model for real data sets (not magnitude).
    T1 model for cases without full T1 recovery, for TR < 5*T1

--- T1IR_orig.py
+++ T1IR_orig.py
@@ -4,9 +4,9 @@ import lmfit
 import numpy as np
 
 
-def initializeT1IR (nroi=None,TI=None, data=None, roi = None, useROIs = False):
+def initializeT1IR (nroi=None,TI=None, data=None, TR=10000, roi = None, useROIs = False):
     """initialize parameters for T1IR absolute value model"""
-    nT1IRparams =3      #max number of parameters, some may be fixed
+    nT1IRparams = 4      #max number of parameters, some may be fixed
     if nroi == None:    #if no parameters are passed return the number of fitting parameters for this model
       return nT1IRparams
     T1params = lmfit.Parameters()   #define parameter dictionary
@@ -18,6 +18,8 @@ def initializeT1IR (nroi=None,TI=None, data=None, roi = None, useROIs = False):
       T1guess=TI[np.argmin(data)]/np.log(2) #minimum signal should occur at ln(2)T1
     T1params.add('T1', value= T1guess, min=0, vary = True)
     paramlist.append('T1')
+    T1params.add('TR', value= TR, vary = False)
+    paramlist.append('TR')
     T1params.add('Si', value= np.amax(data), vary = True)
     paramlist.append('Si')
     T1params.add('B',  value= 2,  min=1.5, max=2.5, vary = True)
@@ -30,12 +32,14 @@ def T1IR(params, TI, data):
     B = params['B'].value
     Si = params['Si'].value
     T1 = params['T1'].value
+    TR = params['TR'].value
 
     model = Si*(1-B * np.exp(-TI/T1))
+    # or to account for non-infinite TR: model = Si*(1+(B-1)*np.exp(-TR/T1)-B*np.exp(-TI/T1))
     return (model - data)
 
 def fitT1IR(params, TI, data):
-    """fits signal vs TI data to T1IRabs model"""
-    result = lmfit.minimize(T1IRabs, params, args=(TI, data))
+    """fits signal vs TI data to T1IR model"""
+    result = lmfit.minimize(T1IR, params, args=(TI, data))
     final = data + result.residual
     return final
